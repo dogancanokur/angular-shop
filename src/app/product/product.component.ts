7548b62d3315430be7902153b137aae27588ea60
dogancanokur <ahmet.okur.94@gmail.com>
2019-07-21 23:55:37 +0300

    created components

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+  title = "Ürün Listesi";
+  products: any[] = [
+    // products : Product[] =
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus ZenBook"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 25,
+      categoryId: 2,
+      description: "A4Tech"
+    }
+  ]
+
+  ngOnInit() {
+  }
+
+}
