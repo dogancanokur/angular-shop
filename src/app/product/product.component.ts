02653df88031eb2c7d347d4eb38dff95cc388b55
dogancanokur <ahmet.okur.94@gmail.com>
2019-07-22 10:35:14 +0300

    custom-pipe

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,6 +10,7 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title = "Ürün Listesi";
+  filterText = "";
   products: Product[] = [
     // products : Product[] =
     {
@@ -18,7 +19,7 @@ export class ProductComponent implements OnInit {
       price: 2500,
       categoryId: 1,
       description: "HP Laptop",
-      imageUrl:"https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=693&q=80"
+      imageUrl: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=693&q=80"
     },
     {
       id: 2,
@@ -26,7 +27,31 @@ export class ProductComponent implements OnInit {
       price: 25,
       categoryId: 2,
       description: "A4Tech",
-      imageUrl:"https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
+      imageUrl: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
+    },
+    {
+      id: 3,
+      name: "Gitar",
+      price: 150,
+      categoryId: 2,
+      description: "Fender",
+      imageUrl: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
+    },
+    {
+      id: 4,
+      name: "Klavye",
+      price: 25,
+      categoryId: 2,
+      description: "Asus",
+      imageUrl: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
+    },
+    {
+      id: 5,
+      name: "Telefon",
+      price: 1000,
+      categoryId: 2,
+      description: "iPhone",
+      imageUrl: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
     }
   ]
 
